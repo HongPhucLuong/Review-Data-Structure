3fa277507f7814a8d4b074f62e5c151d69235ad5
lamphucnghi313 <lamphucnghi@gmail.com>
2020-06-05 23:55:05 +0700

    no message

--- LinkedList/LinkedList_Class.py
+++ LinkedList/LinkedList_Class.py
@@ -22,9 +22,37 @@ class LinkedList:
             pTemp = pTemp.pNext
         pTemp.pNext = Node(newValue)
 
+    # add vi tri bat ki
+    # linkedlist se k dung index
+    def _add_after_value(self, newValue, value1):
+        if self.pHead is None:
+            print('LinkedList is not exists!')
+            return
+        pTemp = self.pHead
+        while pTemp:
+            if pTemp.data == value1:
+                newNode = Node(newValue)
+                newNode.pNext = pTemp.pNext
+                pTemp.pNext = newNode
+            pTemp = pTemp.pNext
+
+
     #delete value
     def _delete_value(self, value):
-        pass
+        if self.pHead is None:
+            print('LinkedList is not exists!')
+            return
+        pTemp = self.pHead
+        pPre = pTemp
+        while pTemp:
+            if pTemp.data != value:
+                pPre = pTemp
+                pTemp = pTemp.pNext
+            else:
+                pDel = pTemp
+                pPre.pNext = pTemp.pNext
+                pTemp = pTemp.pNext
+                del pDel
 
     #delete index
     def _delete_index(self, index):
@@ -40,6 +68,10 @@ class LinkedList:
         return listVal
 
     def _get_len(self) -> int:
+        if self.pHead is None:
+            return 0
+        if self.pHead.pNext is None:
+            return 1
         count = 1
         pTemp = self.pHead
         while pTemp.pNext:
@@ -50,5 +82,17 @@ class LinkedList:
 
 def test():
     myLinkedList = LinkedList()
+    # [myLinkedList._add_at_begining(i) for i in range(10)]
+    # [myLinkedList._add_at_begining(i) for i in range(10)]
+    # myLinkedList._add_after_value(100, 9)
+    # myLinkedList._add_after_value(100, 2)
+    # myLinkedList._print_list_left_right()
+    # print('len={}'.format(myLinkedList._get_len()))
+    # myLinkedList._delete_value(0)
+    # myLinkedList._delete_value(5)
+    # myLinkedList._print_list_left_right()
+    # print('len={}'.format(myLinkedList._get_len()))
+
+    
 
 test()
